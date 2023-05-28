d0614f6eb4687d8ca82572379c7f0ec3f8142f99
MaxSorokin <askonev007@gmail.com>
2023-05-29 01:02:49 +0300

    chore: generate card component & use Material tmp

--- src/app/pages/products-list/card/card.module.ts
+++ src/app/pages/products-list/card/card.module.ts
@@ -1,8 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {CardComponent} from './card.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
-    declarations: [],
-    imports: [CommonModule],
+    declarations: [CardComponent],
+    imports: [CommonModule, MatButtonModule, MatCardModule],
+    exports: [CardComponent],
 })
 export class CardModule {}

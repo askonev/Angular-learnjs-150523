34f8206c89a0dd76007e370a3f178dc2cb1701d7
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-05-29 19:43:44 +0300

    feat: lecture 3 homework

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,8 +1,17 @@
 import {Component} from '@angular/core';
+import {productsMock} from '../../shared/products/products.mock';
+import {IProduct} from '../../shared/products/product.interface';
 
 @Component({
     selector: 'app-products-list',
     templateUrl: './products-list.component.html',
     styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    readonly products = productsMock;
+
+    onProductBuy(id: IProduct['_id']) {
+        // eslint-disable-next-line no-console
+        console.log(id);
+    }
+}

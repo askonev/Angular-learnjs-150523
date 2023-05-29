aa1dd19f3a3f69ccde84d654dfe9cefc5793eb76
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-05-29 19:52:51 +0300

    chore: sanitize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {applicationConfigMock} from './shared/application-config/application-config.mock';
-import {SidenavComponent} from './components/sidenav/sidenav.component';
 
 @Component({
     selector: 'app-root',
@@ -8,22 +7,5 @@ import {SidenavComponent} from './components/sidenav/sidenav.component';
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-    // title = 'Angular-learnjs-150523';
     readonly applicationConfig = applicationConfigMock;
-
-    @ViewChild('sidenav')
-    private readonly sidenavComponent: SidenavComponent | undefined;
-
-    // isSidenavOpenedApp = false;
-
-    onMenuClickApp() {
-        // eslint-disable-next-line no-console
-        // console.log('Menu click event in app component', clientXObj);
-        // this.isSidenavOpenedApp = !this.isSidenavOpenedApp;
-        this.sidenavComponent?.toggleSidenavOpened();
-    }
-
-    // onSidenavOpenedChange(newIsSidenavOpened: boolean) {
-    //     this.isSidenavOpenedApp = newIsSidenavOpened;
-    // }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,16 +7,7 @@ import {IApplicationConfig} from '../../shared/application-config/application-co
     styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-    // readonly title = 'Angular-learnjs-150523';
-    // readonly iconSrc = '../../../favicon.ico';
     @Input() applicationConfig: IApplicationConfig | null = null;
 
-    @Output() menuClickOutput = new EventEmitter<{clientX: number}>();
-
-    onMenuClickHeader(event: MouseEvent) {
-        // eslint-disable-next-line no-console
-        console.log('Menu click');
-        // this.menuClick.next(event.clientX);
-        this.menuClickOutput.emit({clientX: event.clientX});
-    }
+    @Output() menuClickOutput = new EventEmitter<void>();
 }

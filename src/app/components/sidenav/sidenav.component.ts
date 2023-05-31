775f1141401599a5a93da833249af25abf201f66
MaxSorokin <askonev007@gmail.com>
2023-05-31 19:39:11 +0300

    chore: read ElementRef via ViewChild param

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {MatDrawer} from '@angular/material/sidenav';
 
 @Component({
@@ -7,9 +7,16 @@ import {MatDrawer} from '@angular/material/sidenav';
     styleUrls: ['./sidenav.component.css'],
 })
 export class SidenavComponent {
-    @ViewChild('drawer') private readonly matDrawerComponent: MatDrawer | undefined;
+    // @ViewChild('drawer')
+    @ViewChild(MatDrawer, {read: MatDrawer})
+    private readonly matDrawerComponent: MatDrawer | undefined;
+
+    @ViewChild(MatDrawer, {read: ElementRef})
+    private readonly matDrawerElementRef: ElementRef<HTMLElement> | undefined;
 
     toggleSidenavOpened() {
+        // eslint-disable-next-line no-console
+        console.log(this.matDrawerElementRef?.nativeElement);
         this.matDrawerComponent?.toggle();
     }
 }

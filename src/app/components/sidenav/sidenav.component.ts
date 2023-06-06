d2538d1859b74fa93bc53f343de5f11dd9ca6a5a
MaxSorokin <askonev007@gmail.com>
2023-06-06 23:41:38 +0300

    feat: commint test data

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {MatDrawer} from '@angular/material/sidenav';
 
 @Component({
@@ -6,35 +6,35 @@ import {MatDrawer} from '@angular/material/sidenav';
     templateUrl: './sidenav.component.html',
     styleUrls: ['./sidenav.component.css'],
 })
-export class SidenavComponent implements OnInit, AfterViewInit {
-    @ViewChild(MatDrawer, {static: false})
+export class SidenavComponent {
+    @ViewChild(MatDrawer, {static: true})
     private readonly matDrawerComponent: MatDrawer | undefined;
 
-    @ViewChild('test', {static: false})
-    private readonly test: ElementRef | undefined;
+    // @ViewChild('test', {static: false})
+    // private readonly test: ElementRef | undefined;
 
-    @ViewChild('testStatic', {static: true})
-    private readonly testStatic: ElementRef | undefined;
+    // @ViewChild('testStatic', {static: true})
+    // private readonly testStatic: ElementRef | undefined;
 
-    ngOnInit(): void {
-        // eslint-disable-next-line no-console
-        console.log('OnInit', this.test);
-        // eslint-disable-next-line no-console
-        console.log('OnInit', this.testStatic);
-    }
+    // ngOnInit(): void {
+    //     // eslint-disable-next-line no-console
+    //     console.log('OnInit', this.test);
+    //     // eslint-disable-next-line no-console
+    //     console.log('OnInit', this.testStatic);
+    // }
 
-    ngAfterViewInit(): void {
-        // eslint-disable-next-line no-console
-        console.log('AfterViewInit sidenav', this.test);
-    }
+    // ngAfterViewInit(): void {
+    //     // eslint-disable-next-line no-console
+    //     console.log('AfterViewInit sidenav', this.test);
+    // }
 
     isView = true;
 
     toggleSidenavOpened() {
         // eslint-disable-next-line no-console
-        console.log('toggleSidenavOpened', this.test);
+        // console.log('toggleSidenavOpened', this.test);
 
-        this.isView = !this.isView;
+        // this.isView = !this.isView;
         this.matDrawerComponent?.toggle();
     }
 }

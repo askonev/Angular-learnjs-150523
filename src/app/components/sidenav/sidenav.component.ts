76bf104da67d44bbcb63b4e12af9a62f875aa5ac
MaxSorokin <askonev007@gmail.com>
2023-06-07 21:56:06 +0300

    chore: manually create ng-content derectives

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,11 @@
-import {Component, Input, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
+import {
+    Component,
+    ContentChild,
+    OnInit,
+    TemplateRef,
+    ViewChild,
+    ViewContainerRef,
+} from '@angular/core';
 import {MatDrawer} from '@angular/material/sidenav';
 
 @Component({
@@ -6,16 +13,26 @@ import {MatDrawer} from '@angular/material/sidenav';
     templateUrl: './sidenav.component.html',
     styleUrls: ['./sidenav.component.css'],
 })
-export class SidenavComponent {
-    @Input() set listTemplate(template: TemplateRef<unknown>) {
-        this.insertListTemplate(template);
-    }
+export class SidenavComponent implements OnInit {
+    // @Input() set listTemplate(template: TemplateRef<unknown>) {
+    //     this.insertListTemplate(template);
+    // }
 
     @ViewChild(MatDrawer, {static: true})
-    private readonly matDrawerComponent: MatDrawer | undefined;
+    private readonly matDrawerComponent!: MatDrawer;
 
     @ViewChild('listViewpotr', {read: ViewContainerRef, static: true})
     private readonly listViewpotr!: ViewContainerRef;
+
+    // @ContentChild(MatList, {static: true})
+    // private readonly matList!: MatList;
+
+    @ContentChild('listTemplate', {static: true})
+    private readonly listTemplate!: TemplateRef<unknown>;
+
+    ngOnInit(): void {
+        this.insertListTemplate(this.listTemplate);
+    }
 
     // ngOnChanges({listTemplate}: SimpleChanges): void {
     //     if (listTemplate) {
@@ -25,6 +42,8 @@ export class SidenavComponent {
 
     private insertListTemplate(listTemplate: TemplateRef<unknown>) {
         this.listViewpotr.createEmbeddedView(listTemplate);
+        // eslint-disable-next-line no-console
+        console.log(this.listViewpotr);
     }
 
     // @ViewChild('test', {static: false})
@@ -45,13 +64,15 @@ export class SidenavComponent {
     //     console.log('AfterViewInit sidenav', this.test);
     // }
 
-    isView = true;
+    // isView = true;
 
     toggleSidenavOpened() {
         // eslint-disable-next-line no-console
         // console.log('toggleSidenavOpened', this.test);
 
         // this.isView = !this.isView;
+        // eslint-disable-next-line no-console
+        // console.log(this.matList);
         this.matDrawerComponent?.toggle();
     }
 }
